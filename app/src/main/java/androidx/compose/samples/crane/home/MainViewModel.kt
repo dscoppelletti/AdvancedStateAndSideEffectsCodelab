a1f95806416cb3426e6e33a0a859117cc4c4ecba
Dario Scoppelletti <dario@scoppelletti.it>
2024-01-13 11:01:28 +0100

    3. UI state production pipeline

--- app/src/main/java/androidx/compose/samples/crane/home/MainViewModel.kt
+++ app/src/main/java/androidx/compose/samples/crane/home/MainViewModel.kt
@@ -23,6 +23,9 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
@@ -39,18 +42,32 @@ class MainViewModel @Inject constructor(
     val hotels: List<ExploreModel> = destinationsRepository.hotels
     val restaurants: List<ExploreModel> = destinationsRepository.restaurants
 
+    /* BEGIN-3 - UI state production pipeline */
+    private val _suggestedDestinations =
+        MutableStateFlow<List<ExploreModel>>(emptyList())
+
+    val suggestedDestinations: StateFlow<List<ExploreModel>> =
+        _suggestedDestinations.asStateFlow()
+
+    init {
+        _suggestedDestinations.value = destinationsRepository.destinations
+    }
+    /* END-3 */
+
     fun updatePeople(people: Int) {
         viewModelScope.launch {
             if (people > MAX_PEOPLE) {
-            // TODO Codelab: Uncomment
-            //  _suggestedDestinations.value = emptyList()
+                /* BEGIN-3 - UI state production pipeline */
+                _suggestedDestinations.value = emptyList()
+                /* END-3 */
             } else {
                 val newDestinations = withContext(defaultDispatcher) {
                     destinationsRepository.destinations
                         .shuffled(Random(people * (1..100).shuffled().first()))
                 }
-                // TODO Codelab: Uncomment
-                //  _suggestedDestinations.value = newDestinations
+                /* BEGIN-3 - UI state production pipeline */
+                _suggestedDestinations.value = newDestinations
+                /* END-3 */
             }
         }
     }
@@ -61,8 +78,9 @@ class MainViewModel @Inject constructor(
                 destinationsRepository.destinations
                     .filter { it.city.nameToDisplay.contains(newDestination) }
             }
-            // TODO Codelab: Uncomment
-            //  _suggestedDestinations.value = newDestinations
+            /* BEGIN-3 - UI state production pipeline */
+            _suggestedDestinations.value = newDestinations
+            /* END-3 */
         }
     }
 }
